11a8acf1d405e88b90f9ba218b2a0d8078402ef5
shunak <shuna966@gmail.com>
2023-12-08 17:11:18 +0900

    Copilot test#2

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,17 @@
-def fibonacci(n):
-    if n == 0 or n == 1:
+# normal fibonacci
+def fib(n):
+    if n <= 1:
         return n
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fib(n-1) + fib(n-2)
+
+# memoized fibonacci
+def fib_memo(n, memo):
+    if n <= 1:
+        return n
+    elif memo[n] != None:
+        return memo[n]
+    else:
+        memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
+        return memo[n]
 
